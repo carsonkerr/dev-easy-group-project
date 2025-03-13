e9600aa42053ca6cdfb01d9f50ad99f2718a4a6c
Reed Willis <142549714+ReedWillis@users.noreply.github.com>
2025-03-13 12:20:19 -0500

    Add files via upload

---
+++ scripts/card-script.js
@@ -0,0 +1,37 @@
+$(function () {
+    console.log("ready!");
+  
+    $.each(facts, (index, data) => {
+      $("#gameRow").append(`
+      <div class="col-sm-4 mx-auto mt-5">
+          <div class="card">
+            <div class="card-body">${facts[index].fact}</div>
+            <div class="card-footer">
+              <button class="btn btn-primary">Like</button>
+            </div>
+          </div>
+        </div>
+        `);
+    });
+
+
+
+  });
+
+  function search_fact() {
+    let input = document.getElementById('searchBar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('card-body');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "list-item";
+        }
+    }
+
+// Got this ^^^^ from https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
+
+}
